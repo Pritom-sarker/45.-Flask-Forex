1c80167b3f39e3e63a32e5db8d5f8fbf50a8463a
Pritom sarker Bishal <33182615+Pritom-sarker@users.noreply.github.com>
2023-02-11 02:15:47 +0600

    feat: final

---
+++ app.py
@@ -0,0 +1,28 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+from telethon import TelegramClient, events
+import threading
+
+
+app = Flask(__name__)
+ 
+ 
+ 
+ 
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    f = open('data.txt','r')
+    s = ''
+    for line in f:
+        s+=line
+    f.close()
+    return s
+
+    
+# main driver function
+if __name__ == '__main__':
+    app.run()
+    
+	
---
+++ bot.py
@@ -0,0 +1,61 @@
+from telethon import TelegramClient, events
+import time
+
+def writeText(textt):
+    print('')
+    s = ''
+    lines =  textt.split('\n')
+    for line in lines:
+        if "ORDER" in line:
+            s = line
+            print(s)
+            break
+        
+    while True:
+        try:
+            f = open('data.txt', 'w')
+            f.write(s)
+            f.close()
+            break
+        except:
+            time.sleep(1)
+            pass
+
+def get_message_from_telegram():
+    global oldText
+    our_chanel = 1201000242
+    @client.on(events.NewMessage(chats=our_chanel))
+    async def my_event_handler(event):
+        s = event.raw_text
+        print(s)
+        if str(s) in oldText:
+            print('Same text!!')
+        else:
+            oldText.append(s)
+            if 'ORDER'.lower() in s.lower():
+                time.sleep(3)
+                writeText(s)
+            else:
+                print('Ignored!!!')
+        print('----------------------')
+
+if __name__ == '__main__':
+    
+    oldText = []
+    id = '7245145'
+    hash = "962930c707a46df8f9f6f31244c502c5"
+    telApiName = 'newApi11'
+    hour = 10
+    print('Bot wll sleep for next ',hour,"Hours")
+    # time.s/r * 3600)
+    print('bot is running!!')
+
+    client = TelegramClient(telApiName, id, hash)
+    # tel account
+    while True:
+        try:
+            client.start()
+            get_message_from_telegram()
+            client.run_until_disconnected()
+        except Exception as e:
+            print('ERROR SO AGAIN!!!',e)

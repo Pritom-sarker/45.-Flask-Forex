c7898cb5d0793670f23975a99fa5c87e7ae2ef41
Pritom sarker Bishal <33182615+Pritom-sarker@users.noreply.github.com>
2023-02-16 02:13:30 +0600

    feat: first test

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, request, render_template, session, redirect
 from telethon import TelegramClient, events
-import threading
+from threading import Thread
+from datetime import datetime
+import pandas as pd
 
 
 app = Flask(__name__)
  
- 
+@app.route('/hist')
+def All_data():
+    global all_data
+    
+    df = pd.DataFrame(all_data,columns=['Datetime','Text'])
+    df = df.sort_values(by=['Datetime'], ascending=False)
+    return df.to_html(header="true", table_id="table")
+
+
+
  
  
 @app.route('/')
@@ -20,9 +31,28 @@ def hello_world():
     f.close()
     return s
 
-    
+def read_file():
+    while True:
+        try:
+            f = open('data.txt','r')
+            s = ''
+            for line in f:
+                s+=line
+            if s not in text_list:
+                text_list.append(s)
+                all_data.append([datetime.now().strftime("%d/%m/%Y %H:%M:%S"), s])
+                print(all_data)
+            f.close()
+        except:
+            print('Error!!')
+
 # main driver function
 if __name__ == '__main__':
-    app.run(host= "199.247.31.183" , port="80")
+    all_data = [ ]
+    text_list = []
+    thread = Thread(target = read_file)
+    thread.start()
+   # app.run(host= "199.247.31.183" , port="80")
+    app.run(host='0.0.0.0' , port='80')
     
 	
